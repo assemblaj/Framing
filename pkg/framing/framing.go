951fc79e6e391580c305c316be09de5db6489a5d
arheard <arheard0@gmail.com>
2019-03-26 18:51:21 -0400

    Updates Framing struct and adds fastjson library code

--- pkg/framing/framing.go
+++ pkg/framing/framing.go
@@ -1,32 +1,69 @@
 package framing
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 
+	"github.com/assemblaj/fastjson"
 	json "github.com/assemblaj/fastjson"
 )
 
 type DB struct {
-	storage map[string]*Frame
+	storage map[string][]Frame
 }
 
-func (db *DB) Get(value string) (bool, *Frame) {
+func (db *DB) Get(value string) (bool, *[]Frame) {
 	frame, exists := db.storage[value]
-	return exists, frame
+	return exists, &frame
+}
+
+func (db *DB) Load(input io.Reader) error {
+	b, err := ioutil.ReadAll(input)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	data := string(b[:])
+
+	p := json.MappedParser()
+	rawData := p.GetFramingData(data)
+	if rawData == nil {
+		return errors.New("Parsing Error")
+	}
+
+	db.storage = buildFrames(rawData)
+	return nil
 }
 
 type Frame struct {
-	Id        string
+	ID        string
 	MetaData  []string
-	Ancestors *Frame
+	data      *json.Value
+	Ancestors []string
 }
 
-func (db *DB) Load(data string) {
-	p := json.MappedParser()
+func (f *Frame) Get(key string) string {
+	var data []byte
+	data = f.data.Get(key).MarshalTo(data)
+	return string(data[:])
+}
 
-	v, err := p.Parse(data)
-	if err != nil {
-		fmt.Println(err)
+func buildFrames(rawData map[string][]*fastjson.Context) map[string][]Frame {
+	frameMap := make(map[string][]Frame)
+	for value, contexts := range rawData {
+		for _, context := range contexts {
+			frameMap[value] = append(frameMap[value], buildFrame(context))
+		}
 	}
-	fmt.Println(v.String())
+	return frameMap
+}
+
+func buildFrame(context *json.Context) Frame {
+	return Frame{
+		ID:        context.GetKey(),
+		MetaData:  context.GetKeys(),
+		data:      context.GetParent(),
+		Ancestors: context.GetAncestors()}
 }
