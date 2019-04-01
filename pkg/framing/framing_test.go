e22b1e8e2f56fa8e29591d3291e0050d61b5db15
arheard <arheard0@gmail.com>
2019-03-31 22:36:34 -0400

    Adds new tests

--- pkg/framing/framing_test.go
+++ pkg/framing/framing_test.go
@@ -1,43 +1,38 @@
 package framing
 
 import (
-	"strings"
+	"os"
 	"testing"
 )
 
-var smallJSON = `
-		{"menu": {
-			"id": "file",
-			"value": "File",
-			"popup": {
-			"menuitem": [
-				{"value": "New", "onclick": "CreateNewDoc()"},
-				{"value": "Open", "onclick": "OpenDoc()"},
-				{"value": "Close", "onclick": "CloseDoc()"}
-			]
-			}
-		}}	
-`
+var basicTestData = "../../testdata/small.json"
 
 func TestLoad(t *testing.T) {
-	s := strings.NewReader(smallJSON)
+	r, err := os.Open(basicTestData)
+	if err != nil {
+		t.Fatalf("Unable to open file. ")
+	}
+
 	Framing := NewFramingDB()
-	err := Framing.Load(s)
+	err = Framing.Load(r)
 	if err != nil {
 		t.Fatalf("Framing data load failure. ")
 	}
 }
 
 func TestGetFramingMap(t *testing.T) {
-	s := strings.NewReader(smallJSON)
+	r, err := os.Open(basicTestData)
+	if err != nil {
+		t.Fatalf("Unable to open file. ")
+	}
 
 	Framing := NewFramingDB()
-	err := Framing.Load(s)
+	err = Framing.Load(r)
 	if err != nil {
 		t.Fatalf("Framing data load failure. ")
 	}
 
-	exists, _ := Framing.Get("file")
+	exists, _ := Framing.Get(SearchParams{value: "file"})
 	if !exists {
 		t.Fatalf("Frames do not exist for value 'file' ")
 	}
@@ -45,15 +40,18 @@ func TestGetFramingMap(t *testing.T) {
 }
 
 func TestGetValue(t *testing.T) {
-	s := strings.NewReader(smallJSON)
+	r, err := os.Open(basicTestData)
+	if err != nil {
+		t.Fatalf("Unable to open file. ")
+	}
 
 	Framing := NewFramingDB()
-	err := Framing.Load(s)
+	err = Framing.Load(r)
 	if err != nil {
 		t.Fatalf("Framing data load failure. ")
 	}
 
-	exists, frames := Framing.Get("file")
+	exists, frames := Framing.Get(SearchParams{value: "file"})
 	if !exists {
 		t.Fatalf("Frames do not exist for value 'file' ")
 	}
@@ -61,5 +59,85 @@ func TestGetValue(t *testing.T) {
 	exists, _ = (*frames)[0].Get("popup")
 	if exists == false {
 		t.Fatalf("Value not found.")
+	}
+}
+
+func TestMetaDataConvert(t *testing.T) {
+	tst := []string{"1", "2", "3", "4"}
+	tmds := MetaDataString(&tst)
+	tmdsl := MetaDataSlice(tmds)
+	tmds2 := MetaDataString(&tmdsl)
+	if tmds != tmds2 {
+		t.Fatalf("Value not found.")
+	}
+}
+
+var complexTestData = "../../testdata/complex.json"
+
+func TestGetDistincMetaData(t *testing.T) {
+	r, err := os.Open(complexTestData)
+	if err != nil {
+		t.Fatalf("Unable to open file. ")
+	}
+
+	Framing := NewFramingDB()
+	err = Framing.Load(r)
+	if err != nil {
+		t.Fatalf("Framing data load failure. ")
+	}
+
+	fmap := Framing.GetDistincMetaData()
+	if len(fmap) != 0 {
+		t.Fatalf("Result map shouldn't have frames for complex.json")
+	}
+
+}
+
+func TestGetDistinct(t *testing.T) {
+	r, err := os.Open(complexTestData)
+	if err != nil {
+		t.Fatalf("Unable to open file. ")
+	}
+
+	Framing := NewFramingDB()
+	err = Framing.Load(r)
+	if err != nil {
+		t.Fatalf("Framing data load failure. ")
+	}
+
+	e, _ := Framing.GetDistinct("Apple")
+	if !e {
+		t.Fatalf("Value not found . ")
+	}
+}
+
+func TestGroupByMetaData(t *testing.T) {
+	r, err := os.Open(complexTestData)
+	if err != nil {
+		t.Fatalf("Unable to open file. ")
+	}
+
+	Framing := NewFramingDB()
+	err = Framing.Load(r)
+	if err != nil {
+		t.Fatalf("Framing data load failure. ")
+	}
+
+	e, _ := Framing.GroupByMetaData("Apple")
+	if !e {
+		t.Fatalf("Value not found . ")
+	}
+}
+
+func TestSearch(t *testing.T) {
+	r, err := os.Open(complexTestData)
+	if err != nil {
+		t.Fatalf("Unable to open file. ")
+	}
+
+	Framing := NewFramingDB()
+	err = Framing.Load(r)
+	if err != nil {
+		t.Fatalf("Framing data load failure. ")
 	}
 }
